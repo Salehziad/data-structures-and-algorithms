b179515a84cf84e71094f58ccf76e7228171829e
Salehziad <salehziad1999@gmail.com>
2022-06-05 08:28:43 +0300

    !

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,61 @@
-'use strict';
+const { listenerCount } = require('process');
+const LinkedList = require('../index');
 
-// Require our linked list implementation
-const LinkedLilst = require('../index');
-
-describe('Linked List', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
+describe('', () => {
+  test.skip('Can successfully instantiate an empty linked list', () => {
+    const ll = new LinkedList();
+    expect(ll.head).toBeNull();
+  });
+  test.skip('Can properly insert into the linked list', () => {
+    const ll = new LinkedList();
+    ll.insertFirst('first');
+    expect(ll.head.data).toBe('first');
+    expect(ll.head.next).toBeNull();
+    expect(ll.size).toBe(1);
+  });
+  test.skip('The head property will properly point to the first node in the linked list', () => {
+    const ll = new LinkedList();
+    ll.insertFirst('first');
+    ll.insertFirst('second');
+    expect(ll.head.data).toBe('second');
+    expect(ll.head.next.data).toBe('first');
+    expect(ll.head.next.next).toBeNull();
+    expect(ll.size).toBe(2);
+  });
+  test.skip('Can properly insert multiple nodes into the linked list', () => {
+    const ll = new LinkedList();
+    ll.insertFirst('1');
+    ll.insertFirst('2');
+    ll.insertFirst('3');
+    ll.insertFirst('4');
+    ll.insertFirst(1);
+    ll.insertFirst(2);
+    ll.insertFirst(3);
+    ll.insertFirst(4);
+    expect(ll.size).toBe(8);
+  });
+  test('Will return true when finding a value within the linked list that exists', () => {
+    const ll = new LinkedList();
+    ll.insertFirst('first');
+    ll.insertFirst('second');
+    ll.insertFirst(3);
+    ll.insertFirst(4);
+    expect(ll.getSpecificData('first')).toBe(true);
+    expect(ll.getSpecificData(4)).toBe(true);
+  });
+  test('Will return false when searching for a value in the linked list that does not exist', () => {
+    const ll = new LinkedList();
+    ll.insertFirst('first');
+    ll.insertFirst('second');
+    ll.insertFirst(3);
+    ll.insertFirst(4);
+    expect(ll.getSpecificData('firstone')).toBe(false);
+    expect(ll.getSpecificData(444)).toBe(false);
+  });
+  test('Can properly return a collection of all the values that exist in the linked list', () => {
+    const ll = new LinkedList();
+    ll.insertFirst('first');
+    ll.insertFirst('second');
+    expect(ll.printListData()).toBe('{second} -> {first} -> NULL');
   });
 });
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,155 @@
-'use strict';
+/* eslint-disable no-unused-vars */
+// const n1={
+//     data:100
+// };
+// const n2={
+//     data:200
+// };
+// n1.next=n2;
+// console.log(n1)
+//***************************************** */
+class Node{
+  constructor(data,next=null){
+    this.data=data;
+    this.next=next;
+  }
+}
+// const n1=new Node(100);
+// console.log(n1);
 
-class LinkedList {
+class LinkedList{
+  constructor(){
+    this.head=null;
+    this.size=0;
+  }
+  // insert first node
+  insertFirst(data){
+    this.head=new Node(data,this.head);
+    this.size++;
+  }
+  // insert last node
+  insertLast(data){
+    let node=new Node(data);
+    let current;
+    // if empty make head
+    if(!this.head){
+      this.head=node;
+    }else {
+      current=this.head;
+      while(current.next){
+        current=current.next;
+      }
+      current.next=node;
+    }
+    this.size++;
+  }
+  // insert at index
+  insertAt(data,index){
+    if(index>0 && index>this.size){
+      return;
+    }
+    // if firs index
+    if(index===0){
+      this.head=new Node(data,this.head);
+      return;
+    }
+    const node=new Node(data);
+    let current,previous;
 
+    //set current to first
+    current=this.head;
+    let count =0;
+    while(count<index){
+      previous=current; // node before index
+      count++;
+      current=current.next; // node after index
+    }
+    node.next=current;
+    previous.next=node;
+    this.size++;
+  }
+  // get an index
+  getAt(index){
+    let current=this.head;
+    let count=0;
+
+    while(current){
+      if(count==index){
+        return current.data;
+      }
+      count++;
+      current=current.next;
+    }
+
+  }
+  // get data by it name
+  getSpecificData(data){
+    let current=this.head;
+    let count=0;
+    while(current){
+      if (current.data===data){
+        return true;
+      }else{
+        current=current.next;
+      }
+    }
+    return false;
+  }
+  // remove at index
+  removeAtt(index){
+    if(index>0 && index>this.size){
+      return;
+    }
+    let current=this.head;
+    let previous;
+    let count=0;
+    // remove first
+    if(index===0){
+      this.head=this.next;
+    }else {
+      while(count<index){
+        count++;
+        previous=current;
+        current=current.next;
+      }
+      previous.next=current.next;
+    }
+    this.size--;
+  }
+  // clear list
+  clearList(){
+    this.head=null;
+    this.size=0;
+  }
+  // print list data
+  printListData(){
+    let current=this.head;
+    let allList='';
+    let list='';
+    while(current){
+      list+='{'+current.data+'} -> ';
+      allList=list;
+      current=current.next;
+    }
+
+    allList=allList +'NULL';
+    console.log(allList);
+    return allList;
+  }
 }
 
-module.exports = LinkedList;
+const ll=new LinkedList();
+ll.insertFirst('first');
+ll.insertFirst('second');
+// console.log(getSpecificData("first"));
+// ll.removeAtt(1);
+// ll.getAt(1);
+ll.printListData();
+// ll.insertFirst("first");
+// ll.insertFirst("second");
+// console.log(ll.getAt(0))
+console.log(ll.getSpecificData('second'));
+// console.log(ll.head.next)
+// console.log(ll.head.data)
+// console.log(ll.size)
+module.exports=LinkedList;
