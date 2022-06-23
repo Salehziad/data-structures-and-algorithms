105989877136a49de9b06233dd363716002f0567
Salehziad <salehziad1999@gmail.com>
2022-06-23 21:06:44 +0300

    D

--- javascript/code-challenges/queue/index.js
+++ javascript/code-challenges/queue/index.js
@@ -1,27 +1,6 @@
 'use strict';
 const Queue = require('./lib/queue');
 
-let newQueue = new Queue();
+let newQueue = new Queue(); // code challenge 10
 
-// newQueue.enQueue(1);
-// newQueue.enQueue(2);
-// newQueue.enQueue(3);
 
-// newQueue.enQueue(3);
-// console.log('f',newQueue.peek());
-// newQueue.deQueue();
-// newQueue.deQueue();
-// newQueue.deQueue();
-
-// console.log('s',newQueue.peek());
-
-// console.log(newQueue.front.value);
-// console.log(newQueue.front.next.value);
-// console.log(newQueue.front.next.next.value);
-// newQueue.deQueue();
-// console.log(newQueue.peek());
-// newQueue.deQueue();
-// newQueue.deQueue();
-// newQueue.enQueue();
-
-console.log(newQueue.peek());
