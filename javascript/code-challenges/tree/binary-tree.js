9558fb15178129d47f241c8b2dc2861b34f62007
Salehziad <salehziad1999@gmail.com>
2022-07-03 23:20:26 +0300

    tree

---
+++ javascript/code-challenges/tree/binary-tree.js
@@ -0,0 +1,50 @@
+'use strict';
+module.exports = class binaryTree {
+  constructor(root = null) {
+    this.root = root;
+  }
+  preOrder() {
+    let result = [];
+    let traverse = (node) => {
+    //   console.log('ccc',node);
+      result.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    };
+    traverse(this.root);
+    return result;
+  }
+  inOrder() {
+    let result = [];
+    let traverse = (node) => {
+      if (node.left) traverse(node.left);
+      result.push(node.value);
+      if (node.right) traverse(node.right);
+    };
+    traverse(this.root);
+    return result;
+  }
+  postOrder() {
+    let result = [];
+    let traverse = (node) => {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      result.push(node.value);
+    };
+    traverse(this.root);
+    return result;
+  }
+//  insertNode(node, newNode) {
+//     // console.log('111',newNode);
+//     // console.log('2222',node);
+//     if (newNode.value < node.value) {
+//       if (!node.left) node.left = newNode;
+//       else this.insertNode(node.left, newNode);
+//     } else {
+//       if (!node.right) node.right = newNode;
+//       else this.insertNode(node.right, newNode);
+//     }
+//     return newNode;
+//   }
+};
+
